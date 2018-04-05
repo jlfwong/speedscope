75ab807f504e80a46bd6ce35d93ea3ff4c74af07
Jamie Wong <jamie.lf.wong@gmail.com>
2018-04-04 23:36:21 -0700

    WIP

--- release.sh
+++ release.sh
@@ -6,4 +6,7 @@ rm -rf "$OUTDIR"
 mkdir -p "$OUTDIR"
 cp sample/perf-vertx-stacks-01-collapsed-all.txt "$OUTDIR"
 node_modules/.bin/parcel build index.html --out-dir "$OUTDIR" --detailed-report
-ndoe_modules/.bin/
+
+TMPDIR=`mktemp -d -t speedscope-release`
+pushd $TMPDIR
+git clone --depth 1 git@github.com:jlfwong/speedscope.git -b gh-pages
