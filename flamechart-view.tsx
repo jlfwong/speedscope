jlfwong/speedscope

commit 66d1c2b3fceaa8863aa2931008c361eb8d4ee18f

Switch to Source Code Pro, tweak outlines (#48)
This switches all text to be Source Code Pro instead of Courier for sharper rendering at size 10.

This also changes outline widths to be consistent visual widths on retina and non-retina screens.

flamechart-view.tsx
@@ -192,7 +192,7 @@ export class FlamechartPanZoomView extends ReloadableComponent<FlamechartPanZoom
         color = Colors.DARK_BLUE
       }
 
-      ctx.lineWidth = 2
+      ctx.lineWidth = 2 * devicePixelRatio
       ctx.strokeStyle = color
 
       const physicalViewBounds = configToPhysical.transformRect(this.hoveredLabel.configSpaceBounds)
@@ -208,7 +208,6 @@ export class FlamechartPanZoomView extends ReloadableComponent<FlamechartPanZoom
       FontFamily.MONOSPACE
     }`
     ctx.fillStyle = Colors.DARK_GRAY
-    ctx.textBaseline = 'middle'
 
     const minWidthToRender = cachedMeasureTextWidth(ctx, 'M' + ELLIPSIS + 'M')
     const minConfigSpaceWidthToRender = (
@@ -250,15 +249,17 @@ export class FlamechartPanZoomView extends ReloadableComponent<FlamechartPanZoom
           physicalLabelBounds.width() - 2 * LABEL_PADDING_PX,
         )
 
-        // We specify the middle of the label bounds as the position because we use
-        // ctx.textBaseline = 'middle' above.
+        // Note that this is specifying the position of the starting text
+        // baseline.
         ctx.fillText(
           trimmedText,
           physicalLabelBounds.left() + LABEL_PADDING_PX,
-          Math.round(physicalLabelBounds.top() + physicalLabelBounds.height() / 2),
+          Math.round(
+            physicalLabelBounds.bottom() -
+              (physicalViewSpaceFrameHeight - physicalViewSpaceFontSize) / 2,
+          ),
         )
       }
-
       for (let child of frame.children) {
         renderFrameLabelAndChildren(child, depth + 1)
       }
@@ -268,7 +269,7 @@ export class FlamechartPanZoomView extends ReloadableComponent<FlamechartPanZoom
       renderFrameLabelAndChildren(frame)
     }
 
-    const frameOutlineWidth = 2
+    const frameOutlineWidth = 2 * window.devicePixelRatio
     ctx.strokeStyle = Colors.PALE_DARK_BLUE
     ctx.lineWidth = frameOutlineWidth
     const minConfigSpaceWidthToRenderOutline = (

index.html
@@ -1,169 +1,173 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>speedscope</title>
+  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
   <script>
-// https://github.com/evanw/webgl-recorder
-false && (function() {
-  var getContext = HTMLCanvasElement.prototype.getContext;
-  var requestAnimationFrame = window.requestAnimationFrame;
-  var frameSincePageLoad = 0;
+    // https://github.com/evanw/webgl-recorder
+    false && (function () {
+      var getContext = HTMLCanvasElement.prototype.getContext;
+      var requestAnimationFrame = window.requestAnimationFrame;
+      var frameSincePageLoad = 0;
 
-  function countFrames() {
-    frameSincePageLoad++;
-    requestAnimationFrame(countFrames);
-  }
+      function countFrames() {
+        frameSincePageLoad++;
+        requestAnimationFrame(countFrames);
+      }
 
-  window.requestAnimationFrame = function() {
-    return requestAnimationFrame.apply(window, arguments);
-  };
-
-  HTMLCanvasElement.prototype.getContext = function(type) {
-    var canvas = this;
-    var context = getContext.apply(canvas, arguments);
-
-    if (type === 'webgl' || type === 'experimental-webgl') {
-      var oldWidth = canvas.width;
-      var oldHeight = canvas.height;
-      var oldFrameCount = frameSincePageLoad;
-      var trace = [];
-      var variables = {};
-      var fakeContext = {
-        trace: trace,
-        compileTrace: compileTrace,
-        downloadTrace: downloadTrace,
+      window.requestAnimationFrame = function () {
+        return requestAnimationFrame.apply(window, arguments);
       };
 
-      trace.push('  gl.canvas.width = ' + oldWidth + ';');
-      trace.push('  gl.canvas.height = ' + oldHeight + ';');
+      HTMLCanvasElement.prototype.getContext = function (type) {
+        var canvas = this;
+        var context = getContext.apply(canvas, arguments);
 
-      function compileTrace() {
-        var text = 'function* render(gl) {\n';
-        text += '  // Recorded using https://github.com/evanw/webgl-recorder\n';
-        for (var key in variables) {
-          text += '  var ' + key + 's = [];\n';
-        }
-        text += trace.join('\n');
-        text += '\n}\n';
-        return text;
-      }
+        if (type === 'webgl' || type === 'experimental-webgl') {
+          var oldWidth = canvas.width;
+          var oldHeight = canvas.height;
+          var oldFrameCount = frameSincePageLoad;
+          var trace = [];
+          var variables = {};
+          var fakeContext = {
+            trace: trace,
+            compileTrace: compileTrace,
+            downloadTrace: downloadTrace,
+          };
 
-      function downloadTrace() {
-        var text = compileTrace();
-        var link = document.createElement('a');
-        link.href = URL.createObjectURL(new Blob([text], {type: 'application/javascript'}));
-        link.download = 'trace.js';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }
+          trace.push('  gl.canvas.width = ' + oldWidth + ';');
+          trace.push('  gl.canvas.height = ' + oldHeight + ';');
 
-      function getVariable(value) {
-        if (value instanceof WebGLActiveInfo ||
-            value instanceof WebGLBuffer ||
-            value instanceof WebGLFramebuffer ||
-            value instanceof WebGLProgram ||
-            value instanceof WebGLRenderbuffer ||
-            value instanceof WebGLShader ||
-            value instanceof WebGLShaderPrecisionFormat ||
-            value instanceof WebGLTexture ||
-            value instanceof WebGLUniformLocation) {
-          var name = value.constructor.name;
-          var list = variables[name] || (variables[name] = []);
-          var index = list.indexOf(value);
-
-          if (index === -1) {
-            index = list.length;
-            list.push(value);
+          function compileTrace() {
+            var text = 'function* render(gl) {\n';
+            text += '  // Recorded using https://github.com/evanw/webgl-recorder\n';
+            for (var key in variables) {
+              text += '  var ' + key + 's = [];\n';
+            }
+            text += trace.join('\n');
+            text += '\n}\n';
+            return text;
           }
 
-          return name + 's[' + index + ']';
-        }
+          function downloadTrace() {
+            var text = compileTrace();
+            var link = document.createElement('a');
+            link.href = URL.createObjectURL(new Blob([text], { type: 'application/javascript' }));
+            link.download = 'trace.js';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }
 
-        return null;
-      }
+          function getVariable(value) {
+            if (value instanceof WebGLActiveInfo ||
+              value instanceof WebGLBuffer ||
+              value instanceof WebGLFramebuffer ||
+              value instanceof WebGLProgram ||
+              value instanceof WebGLRenderbuffer ||
+              value instanceof WebGLShader ||
+              value instanceof WebGLShaderPrecisionFormat ||
+              value instanceof WebGLTexture ||
+              value instanceof WebGLUniformLocation) {
+              var name = value.constructor.name;
+              var list = variables[name] || (variables[name] = []);
+              var index = list.indexOf(value);
 
-      console.timeStamp('start')
-      var start = performance.now()
-      for (var key in context) {
-        var value = context[key];
-
-        if (typeof value === 'function') {
-          fakeContext[key] = function(key, value) {
-            return function() {
-              trace.push(`// ${performance.now() - start}`)
-              var result = value.apply(context, arguments);
-              var args = [];
-
-              if (frameSincePageLoad !== oldFrameCount) {
-                oldFrameCount = frameSincePageLoad;
-                trace.push('  yield;');
+              if (index === -1) {
+                index = list.length;
+                list.push(value);
               }
 
-              if (canvas.width !== oldWidth || canvas.height !== oldHeight) {
-                oldWidth = canvas.width;
-                oldHeight = canvas.height;
-                trace.push('  gl.canvas.width = ' + oldWidth + ';');
-                trace.push('  gl.canvas.height = ' + oldHeight + ';');
-              }
+              return name + 's[' + index + ']';
+            }
 
-              for (var i = 0; i < arguments.length; i++) {
-                var arg = arguments[i];
+            return null;
+          }
 
-                if (typeof arg === 'number' || typeof arg === 'boolean' || typeof arg === 'string' || arg === null) {
-                  args.push(JSON.stringify(arg));
-                }
+          console.timeStamp('start')
+          var start = performance.now()
+          for (var key in context) {
+            var value = context[key];
 
-                else if (ArrayBuffer.isView(arg)) {
-                  args.push('new ' + arg.constructor.name + '([' + Array.prototype.slice.call(arg) + '])');
-                }
+            if (typeof value === 'function') {
+              fakeContext[key] = function (key, value) {
+                return function () {
+                  trace.push(`// ${performance.now() - start}`)
+                  var result = value.apply(context, arguments);
+                  var args = [];
 
-                else {
-                  var variable = getVariable(arg);
-                  if (variable !== null) {
-                    args.push(variable);
+                  if (frameSincePageLoad !== oldFrameCount) {
+                    oldFrameCount = frameSincePageLoad;
+                    trace.push('  yield;');
                   }
 
-                  else {
-                    console.log('unsupported value:', arg);
-                    args.push('null');
+                  if (canvas.width !== oldWidth || canvas.height !== oldHeight) {
+                    oldWidth = canvas.width;
+                    oldHeight = canvas.height;
+                    trace.push('  gl.canvas.width = ' + oldWidth + ';');
+                    trace.push('  gl.canvas.height = ' + oldHeight + ';');
                   }
-                }
-              }
 
-              var text = 'gl.' + key + '(' + args.join(', ') + ');';
-              var variable = getVariable(result);
-              if (variable !== null) text = variable + ' = ' + text;
-              trace.push('  ' + text);
+                  for (var i = 0; i < arguments.length; i++) {
+                    var arg = arguments[i];
 
-              return result;
-            };
-          }(key, value);
+                    if (typeof arg === 'number' || typeof arg === 'boolean' || typeof arg === 'string' || arg === null) {
+                      args.push(JSON.stringify(arg));
+                    }
+
+                    else if (ArrayBuffer.isView(arg)) {
+                      args.push('new ' + arg.constructor.name + '([' + Array.prototype.slice.call(arg) + '])');
+                    }
+
+                    else {
+                      var variable = getVariable(arg);
+                      if (variable !== null) {
+                        args.push(variable);
+                      }
+
+                      else {
+                        console.log('unsupported value:', arg);
+                        args.push('null');
+                      }
+                    }
+                  }
+
+                  var text = 'gl.' + key + '(' + args.join(', ') + ');';
+                  var variable = getVariable(result);
+                  if (variable !== null) text = variable + ' = ' + text;
+                  trace.push('  ' + text);
+
+                  return result;
+                };
+              }(key, value);
+            }
+
+            else {
+              fakeContext[key] = value;
+            }
+          }
+
+          return fakeContext;
         }
 
-        else {
-          fakeContext[key] = value;
-        }
-      }
+        return context;
+      };
 
-      return fakeContext;
-    }
-
-    return context;
-  };
-
-  countFrames();
-})();
+      countFrames();
+    })();
   </script>
-	<link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="reset.css">
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
 </head>
+
 <body>
   <div id="root"></div>
   <script src="./speedscope.tsx"></script>
 </body>
+
 </html>

style.ts
@@ -1,6 +1,5 @@
-// TODO(jlfwong): Choose a font that's optimized for sharp display at 10px and 20px
 export enum FontFamily {
-  MONOSPACE = 'Courier, monospace',
+  MONOSPACE = '"Source Code Pro", Courier, monospace',
 }
 
 export enum FontSize {

commit 9cdda3abf6cbe56d84f1abf4a71e1ef16077aa7e

Fix position of x axis labels (#50)
This was broken by #48

flamechart-view.tsx
@@ -207,6 +207,7 @@ export class FlamechartPanZoomView extends ReloadableComponent<FlamechartPanZoom
     ctx.font = `${physicalViewSpaceFontSize}px/${physicalViewSpaceFrameHeight}px ${
       FontFamily.MONOSPACE
     }`
+    ctx.textBaseline = 'alphabetic'
     ctx.fillStyle = Colors.DARK_GRAY
 
     const minWidthToRender = cachedMeasureTextWidth(ctx, 'M' + ELLIPSIS + 'M')
@@ -358,6 +359,7 @@ export class FlamechartPanZoomView extends ReloadableComponent<FlamechartPanZoom
       ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'
       ctx.fillRect(0, 0, physicalViewSize.x, physicalViewSpaceFrameHeight)
       ctx.fillStyle = Colors.GRAY
+      ctx.textBaseline = 'top'
       for (let x = Math.ceil(left / interval) * interval; x < right; x += interval) {
         // TODO(jlfwong): Ensure that labels do not overlap
         const pos = Math.round(configToPhysical.transformPosition(new Vec2(x, 0)).x)