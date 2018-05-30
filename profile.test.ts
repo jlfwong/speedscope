24985875eb0411a300d332b419e192370fe2c46c
Jamie Wong <jamie.lf.wong@gmail.com>
2018-05-29 19:39:02 -0700

    Add test for CallTreeProfileBuilder

--- profile.test.ts
+++ profile.test.ts
@@ -1,4 +1,10 @@
-import {FrameInfo, StackListProfileBuilder, CallTreeNode} from './profile'
+import {
+  FrameInfo,
+  StackListProfileBuilder,
+  CallTreeNode,
+  CallTreeProfileBuilder,
+  Profile,
+} from './profile'
 
 function getFrameInfo(key: string): FrameInfo {
   return {
@@ -15,33 +21,7 @@ const fc = getFrameInfo('c')
 const fd = getFrameInfo('d')
 const fe = getFrameInfo('e')
 
-test('StackListProfileBuilder', () => {
-  const b = new StackListProfileBuilder()
-
-  const samples = [
-    // prettier-ignore
-    [fa],
-    [fa, fb],
-    [fa, fb],
-    [fa, fb, fd],
-    [fa, fb, fc],
-    [],
-    [fa],
-    [fa, fb],
-    [fa, fb, fb],
-    [fa, fb, fe],
-    [fa],
-  ]
-
-  samples.forEach(stack => {
-    b.appendSample(stack, 1)
-  })
-  b.appendSample([], 4)
-
-  const profile = b.build()
-  expect(profile.getTotalWeight()).toBe(samples.length + 4)
-  expect(profile.getTotalNonIdleWeight()).toBe(samples.length - 1)
-
+function verifyProfile(profile: Profile) {
   const allFrameKeys = new Set([fa, fb, fc, fd, fe].map(f => f.key))
   const framesInProfile = new Set<string | number>()
   profile.forEachFrame(f => framesInProfile.add(f.key))
@@ -94,4 +74,67 @@ test('StackListProfileBuilder', () => {
     'a;b',
     'a',
   ])
+}
+
+test('StackListProfileBuilder', () => {
+  const b = new StackListProfileBuilder()
+
+  const samples = [
+    // prettier-ignore
+    [fa],
+    [fa, fb],
+    [fa, fb],
+    [fa, fb, fd],
+    [fa, fb, fc],
+    [],
+    [fa],
+    [fa, fb],
+    [fa, fb, fb],
+    [fa, fb, fe],
+    [fa],
+  ]
+
+  samples.forEach(stack => {
+    b.appendSample(stack, 1)
+  })
+  b.appendSample([], 4)
+
+  const profile = b.build()
+  expect(profile.getTotalWeight()).toBe(samples.length + 4)
+  expect(profile.getTotalNonIdleWeight()).toBe(samples.length - 1)
+  verifyProfile(profile)
+})
+
+test('CallTreeProfileBuilder', () => {
+  const b = new CallTreeProfileBuilder()
+
+  b.enterFrame(fa, 0)
+
+  b.enterFrame(fb, 1)
+
+  b.enterFrame(fd, 3)
+
+  b.leaveFrame(fd, 4)
+  b.enterFrame(fc, 4)
+
+  b.leaveFrame(fc, 5)
+  b.leaveFrame(fb, 5)
+  b.leaveFrame(fa, 5)
+
+  b.enterFrame(fa, 6)
+
+  b.enterFrame(fb, 7)
+
+  b.enterFrame(fb, 8)
+
+  b.leaveFrame(fb, 9)
+  b.enterFrame(fe, 9)
+
+  b.leaveFrame(fe, 10)
+  b.leaveFrame(fb, 10)
+
+  b.leaveFrame(fa, 11)
+
+  const profile = b.build()
+  verifyProfile(profile)
 })
