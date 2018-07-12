b5a9228e41da1df9991f28f5c6d3e53126cdefdd
Jamie Wong <jlfwong@figma.com>
2018-07-12 00:37:11 -0700

    Start reviving import of sampled profiles

--- file-format-spec.ts
+++ file-format-spec.ts
@@ -7,7 +7,7 @@ export namespace FileFormat {
     shared: {
       frames: Frame[]
     }
-    profiles: EventedProfile[]
+    profiles: (EventedProfile | SampledProfile)[]
   }
 
   export interface Frame {
@@ -19,16 +19,17 @@ export namespace FileFormat {
 
   export enum ProfileType {
     EVENTED = 'evented',
+    SAMPLED = 'sampled',
   }
 
   export interface IProfile {
+    // Type of profile. This will future proof the file format to allow many
+    // different kinds of profiles to be contained and each type to be part of
+    // a discriminated union.
     type: ProfileType
   }
 
   export interface EventedProfile extends IProfile {
-    // Type of profile. This will future proof the file format to allow many
-    // different kinds of profiles to be contained and each type to be part of
-    // a discriminated union.
     type: ProfileType.EVENTED
 
     // Name of the profile. Typically a filename for the source of the profile.
@@ -51,6 +52,46 @@ export namespace FileFormat {
     // List of events that occured as part of this profile.
     // The "at" field of every event must be in non-decreasing order.
     events: (OpenFrameEvent | CloseFrameEvent)[]
+  }
+
+  interface CallTreeNode {
+    // Index into the frames array
+    frame: number
+
+    // Index into the nodes array
+    parent?: number
+  }
+
+  export interface SampledProfile extends IProfile {
+    type: ProfileType.SAMPLED
+
+    // Name of the profile. Typically a filename for the source of the profile.
+    name: string
+
+    // Unit which all value are specified using in the profile.
+    unit: ValueUnit
+
+    // The starting value of the profile. This will typically be a timestamp.
+    // All event values will be relative to this startValue.
+    startValue: number
+
+    // The final value of the profile. This will typically be a timestamp. This
+    // must be greater than or equal to the startValue. This is useful in
+    // situations where the recorded profile extends past the end of the recorded
+    // events, which may happen if nothing was happening at the end of the
+    // profile.
+    endValue: number
+
+    // List of nodes in the call tree
+    nodes: CallTreeNode[]
+
+    // List of indices into nodes, with -1 indicating that the call-stack
+    // was empty at the time of the sample
+    samples: number[]
+
+    // The weight of the sample at the given index. Should have
+    // the same length as the samples array.
+    weights: number[]
   }
 
   export type ValueUnit =
