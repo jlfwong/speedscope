1f5251c6c70c5783440ff66b2a853f13ac3ae8e2
Jamie Wong <jlfwong@figma.com>
2018-07-21 15:21:26 -0700

    Move to 8.5.0 directory

--- import/v8proflog.test.ts
+++ import/v8proflog.test.ts
@@ -1,11 +1,5 @@
-import * as child_process from 'child_process'
 import {checkProfileSnapshot} from '../test-utils'
 
 test('importFromV8ProfLog', async () => {
-  const isolatePath = './sample/profiles/node/isolate-0x102802600-v8.log'
-  const v8logJsonPath = './sample/profiles/node/simple.v8log.json'
-  child_process.execSync(
-    `${process.execPath} --prof-process --preprocess -j ${isolatePath} > ${v8logJsonPath}`,
-  )
-  await checkProfileSnapshot(v8logJsonPath)
+  await checkProfileSnapshot('./sample/profiles/node/8.5.0/simple.v8log.json')
 })
