f957bd685705de69c15fa8b88d84eaf34e7fb08d
Jamie Wong <jlfwong@figma.com>
2018-08-01 01:35:40 -0700

    Fix test

--- src/lib/demangle-cpp.test.ts
+++ src/lib/demangle-cpp.test.ts
@@ -4,6 +4,6 @@ test('demangleCpp', () => {
   expect(demangleCpp('a')).toBe('a')
   expect(demangleCpp('someUnobfuscatedFunction')).toBe('someUnobfuscatedFunction')
   expect(demangleCpp('__ZNK7Support6ColorFeqERKS0_')).toBe(
-    'Support::ColorF::operator==(Support::ColorF&)',
+    'Support::ColorF::operator==(Support::ColorF const&) const',
   )
 })
