5429dfaf3fca4793fcd61c317f41af70542a5bbb
Jamie Wong <jlfwong@figma.com>
2018-08-14 08:30:35 -0700

    Add check which would have caught this

--- scripts/generate-file-format-schema-json.js
+++ scripts/generate-file-format-schema-json.js
@@ -9,5 +9,11 @@ let jsonSchema = child_process.execSync(
   },
 )
 jsonSchema = JSON.parse(jsonSchema)
+
+if (!jsonSchema.definitions['FileFormat.File']) {
+  console.error('SCHEMA GENERATION FAILURE: Could not find FileFormat.File in the definitions list')
+  process.exit(1)
+}
+
 jsonSchema['$ref'] = '#/definitions/FileFormat.File'
 console.log(JSON.stringify(jsonSchema, null, 4))
