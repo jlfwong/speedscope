4cbbc499b451a1a7a81f039e1de20b5a6575a963
Jamie Wong <jlfwong@figma.com>
2018-08-15 09:01:51 -0700

    Add more format option outputs for perf script

--- sample/programs/cpp/perf-script.sh
+++ sample/programs/cpp/perf-script.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 perf record -F 999 -g ./simple-terminates > perf.data
-perf script -i perf.data
+
+if [[ "$1" == "with-header" ]]; then
+  perf script --header -i perf.data
+elif [[ "$1" == "with-pid" ]]; then
+  # perf script -F comm,pid,tid,time,event,ip,sym,dso -i perf.data
+  # comm: command (maybe truncated)
+  # pid: process ID
+  # id: thread ID
+  # time: time of event
+  # ip: instruction pointer
+  # sym: symbol
+  # dso: dynamic shared object
+  perf script -F comm,pid,tid,time,event,ip,sym,dso -i perf.data
+else
+  perf script -i perf.data
+fi
