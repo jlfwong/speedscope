0aacca553692b84ea277e025b4c4faf3a90ba253
Jamie Wong <jlfwong@figma.com>
2018-08-18 10:50:55 -0700

    Port RowAtlas

---
+++ src/gl2/row-atlas.ts
@@ -0,0 +1,91 @@
+import {LRUCache} from '../lib/lru-cache'
+import {RectangleBatch, RectangleBatchRenderer} from './rectangle-batch-renderer'
+import {Rect, Vec2} from '../lib/math'
+import {Color} from '../lib/color'
+import {Graphics} from './graphics'
+import {TextureRenderer} from './texture-cached-renderer'
+
+export class RowAtlas<K> {
+  private texture: Graphics.Texture
+  private renderTarget: Graphics.RenderTarget
+  private rowCache: LRUCache<K, number>
+  private clearLineBatch: RectangleBatch
+
+  constructor(
+    private gl: Graphics.Context,
+    private rectangleBatchRenderer: RectangleBatchRenderer,
+    private textureRenderer: TextureRenderer,
+  ) {
+    this.texture = gl.createTexture(Graphics.TextureFormat.NEAREST_CLAMP, 4096, 4096)
+    this.renderTarget = gl.createRenderTarget(this.texture)
+    this.rowCache = new LRUCache(this.texture.height)
+    this.clearLineBatch = new RectangleBatch(gl)
+    this.clearLineBatch.addRect(Rect.unit, new Color(0, 0, 0, 0))
+  }
+
+  has(key: K) {
+    return this.rowCache.has(key)
+  }
+  getResolution() {
+    return this.texture.width
+  }
+  getCapacity() {
+    return this.texture.height
+  }
+
+  private allocateLine(key: K): number {
+    if (this.rowCache.getSize() < this.rowCache.getCapacity()) {
+      // Not in cache, but cache isn't full
+      const row = this.rowCache.getSize()
+      this.rowCache.insert(key, row)
+      return row
+    } else {
+      // Not in cache, and cache is full. Evict something.
+      const [, row] = this.rowCache.removeLRU()!
+      this.rowCache.insert(key, row)
+      return row
+    }
+  }
+
+  writeToAtlasIfNeeded(keys: K[], render: (textureDstRect: Rect, key: K) => void) {
+    this.gl.setRenderTarget(this.renderTarget)
+
+    for (let key of keys) {
+      let row = this.rowCache.get(key)
+      if (row != null) {
+        // Already cached!
+        continue
+      }
+      // Not cached -- we'll have to actually render
+      row = this.allocateLine(key)
+
+      const textureRect = new Rect(new Vec2(0, row), new Vec2(this.texture.width, 1))
+      this.rectangleBatchRenderer.render({
+        batch: this.clearLineBatch,
+        configSpaceSrcRect: Rect.unit,
+        physicalSpaceDstRect: textureRect,
+      })
+      render(textureRect, key)
+    }
+
+    this.gl.setRenderTarget(null)
+  }
+
+  renderViaAtlas(key: K, dstRect: Rect): boolean {
+    let row = this.rowCache.get(key)
+    if (row == null) {
+      return false
+    }
+
+    const textureRect = new Rect(new Vec2(0, row), new Vec2(this.texture.width, 1))
+
+    // At this point, we have the row in cache, and we can
+    // paint directly from it into the framebuffer.
+    this.textureRenderer.render({
+      texture: this.texture,
+      srcRect: textureRect,
+      dstRect: dstRect,
+    })
+    return true
+  }
+}
--- src/gl2/texture-cached-renderer.ts
+++ src/gl2/texture-cached-renderer.ts
@@ -1,10 +1,9 @@
-import regl from 'regl'
 import {Vec2, Rect, AffineTransform} from '../lib/math'
 import {Graphics} from './graphics'
 import {setUniformAffineTransform} from './utils'
 
 export interface TextureRendererProps {
-  texture: regl.Texture
+  texture: Graphics.Texture
   srcRect: Rect
   dstRect: Rect
 }
@@ -34,83 +33,13 @@ const frag = `
   }
 `
 
-export class TextureRenderer {
-  private command: regl.Command<TextureRendererProps>
-  constructor(gl: regl.Instance) {
-    this.command = gl({
-      vert,
-      frag,
-
-      depth: {
-        enable: false,
-      },
-
-      attributes: {
-        // Cover full canvas with a rectangle
-        // with 2 triangles using a triangle
-        // strip.
-        //
-        // 0 +--+ 1
-        //   | /|
-        //   |/ |
-        // 2 +--+ 3
-        position: gl.buffer([[-1, 1], [1, 1], [-1, -1], [1, -1]]),
-        uv: gl.buffer([[0, 1], [1, 1], [0, 0], [1, 0]]),
-      },
-
-      uniforms: {
-        texture: (context, props) => props.texture,
-        uvTransform: (context, props) => {
-          const {srcRect, texture} = props
-          const physicalToUV = AffineTransform.withTranslation(new Vec2(0, 1))
-            .times(AffineTransform.withScale(new Vec2(1, -1)))
-            .times(
-              AffineTransform.betweenRects(
-                new Rect(Vec2.zero, new Vec2(texture.width, texture.height)),
-                Rect.unit,
-              ),
-            )
-          const uvRect = physicalToUV.transformRect(srcRect)
-          return AffineTransform.betweenRects(Rect.unit, uvRect).flatten()
-        },
-        positionTransform: (context, props) => {
-          const {dstRect} = props
-
-          const viewportSize = new Vec2(context.viewportWidth, context.viewportHeight)
-
-          const physicalToNDC = AffineTransform.withScale(new Vec2(1, -1)).times(
-            AffineTransform.betweenRects(new Rect(Vec2.zero, viewportSize), Rect.NDC),
-          )
-          const ndcRect = physicalToNDC.transformRect(dstRect)
-          return AffineTransform.betweenRects(Rect.NDC, ndcRect).flatten()
-        },
-      },
-
-      primitive: 'triangle strip',
-
-      count: 4,
-    })
-  }
-
-  render(props: TextureRendererProps) {
-    this.command(props)
-  }
-
-  resetStats() {
-    return Object.assign(this.command.stats, {cpuTime: 0, gpuTime: 0, count: 0})
-  }
-  stats() {
-    return this.command.stats
-  }
-}
-
-export interface Sky_TextureRendererProps {
+export interface TextureRendererProps {
   texture: Graphics.Texture
   srcRect: Rect
   dstRect: Rect
 }
 
-export class Sky_TextureRenderer {
+export class TextureRenderer {
   private buffer: Graphics.VertexBuffer
   private material: Graphics.Material
 
@@ -138,7 +67,7 @@ export class Sky_TextureRenderer {
     this.material = gl.createMaterial(vertexFormat, vert, frag)
   }
 
-  render(props: Sky_TextureRendererProps) {
+  render(props: TextureRendererProps) {
     this.material.setUniformSampler('texture', props.texture, 0)
     setUniformAffineTransform(
       this.material,
@@ -175,89 +104,5 @@ export class Sky_TextureRenderer {
       })(),
     )
     this.gl.draw(Graphics.Primitive.TRIANGLE_STRIP, this.material, this.buffer)
-  }
-}
-
-export interface TextureCachedRendererOptions<T> {
-  textureRenderer: TextureRenderer
-  render(t: T): void
-  shouldUpdate(oldProps: T, newProps: T): boolean
-}
-
-export class TextureCachedRenderer<T> {
-  private renderUncached: (t: T) => void
-  private shouldUpdate: (oldProps: T, newProps: T) => boolean
-  private texture: regl.Texture
-  private framebuffer: regl.Framebuffer
-  private textureRenderer: TextureRenderer
-  private withContext: regl.Command<{}>
-
-  constructor(private gl: regl.Instance, options: TextureCachedRendererOptions<T>) {
-    this.renderUncached = options.render
-    this.shouldUpdate = options.shouldUpdate
-    this.textureRenderer = options.textureRenderer
-
-    this.texture = gl.texture(1, 1)
-    this.framebuffer = gl.framebuffer({color: [this.texture]})
-    this.withContext = gl({})
-  }
-
-  private lastRenderProps: T | null = null
-
-  private dirty: boolean = false
-  setDirty() {
-    this.dirty = true
-  }
-
-  render(props: T) {
-    this.withContext((context: regl.Context) => {
-      let needsRender = false
-      if (
-        this.texture.width !== context.viewportWidth ||
-        this.texture.height !== context.viewportHeight
-      ) {
-        // TODO(jlfwong): Can probably just use this.framebuffer.resize
-        this.texture({width: context.viewportWidth, height: context.viewportHeight})
-        this.framebuffer({color: [this.texture]})
-        needsRender = true
-      } else if (this.lastRenderProps == null) {
-        needsRender = true
-      } else if (this.shouldUpdate(this.lastRenderProps, props)) {
-        needsRender = true
-      } else if (this.dirty) {
-        needsRender = true
-      }
-
-      if (needsRender) {
-        this.gl({
-          viewport: (context, props) => {
-            return {
-              x: 0,
-              y: 0,
-              width: context.viewportWidth,
-              height: context.viewportHeight,
-            }
-          },
-          framebuffer: this.framebuffer,
-        })(() => {
-          this.gl.clear({color: [0, 0, 0, 0]})
-          this.renderUncached(props)
-        })
-      }
-
-      const glViewportRect = new Rect(
-        Vec2.zero,
-        new Vec2(context.viewportWidth, context.viewportHeight),
-      )
-
-      // Render from texture
-      this.textureRenderer.render({
-        texture: this.texture,
-        srcRect: glViewportRect,
-        dstRect: glViewportRect,
-      })
-      this.lastRenderProps = props
-      this.dirty = false
-    })
   }
 }
