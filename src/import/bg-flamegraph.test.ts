2d957b536017bf8a2c844510176844bd5ab1d852
Janusz Nykiel <janusz.nykiel@gmail.com>
2018-09-04 21:00:28 -0400

    Collapsed stack file with CRLF line endings test case

--- src/import/bg-flamegraph.test.ts
+++ src/import/bg-flamegraph.test.ts
@@ -3,3 +3,7 @@ import {checkProfileSnapshot} from '../lib/test-utils'
 test('importFromBGFlameGraph', async () => {
   await checkProfileSnapshot('./sample/profiles/stackcollapse/simple.txt')
 })
+
+test('importFromBGFlameGraphWithCRLF', async () => {
+  await checkProfileSnapshot('./sample/profiles/stackcollapse/simple-crlf.txt')
+})
