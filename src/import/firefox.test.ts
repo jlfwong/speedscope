b6190362b41d0e559463051df846c2a0a09c83f6
Jonathan Chan <jyc@eqv.io>
2018-09-04 17:03:55 -0700

    Match more Firefox-internal locations (#156)
    Looks like Firefox also generates locations with names like
    `bound (self-hosted:951:0)`. We check for `self-hosted`, but not for
    `self-hosted` with stuff after it following a colon. We should ignore
    these too, otherwise we can end up with stuff on our stack that we don't
    expect. This was causing Firefox profiles not to load because we
    completed building the profile with a non-empty stack.
    
    Attached is a profile that errors without this patch and successfully renders
    with this patch.
    
    [copy.json.zip](https://github.com/jlfwong/speedscope/files/2350583/copy.json.zip)

--- src/import/firefox.test.ts
+++ src/import/firefox.test.ts
@@ -7,3 +7,7 @@ test('importFromFirefox', async () => {
 test('importFromFirefox recursion', async () => {
   await checkProfileSnapshot('./sample/profiles/Firefox/61/recursion.json')
 })
+
+test('importFromFirefox ignore self-hosted', async () => {
+  await checkProfileSnapshot('./sample/profiles/Firefox/63/simple-firefox.json')
+})
