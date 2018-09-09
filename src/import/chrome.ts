256b1af2bfdd746e278a42375fac27c8e2af5c99
Jamie Wong <jlfwong@figma.com>
2018-09-09 14:26:17 -0700

    Also accept profiles with Profile but no ProfileChunk

--- src/import/chrome.ts
+++ src/import/chrome.ts
@@ -52,7 +52,12 @@ export function isChromeTimeline(rawProfile: any): boolean {
   if (rawProfile.length < 1) return false
   const first = rawProfile[0]
   if (!('pid' in first && 'tid' in first && 'ph' in first && 'cat' in first)) return false
-  if (!rawProfile.find(e => e.name === 'CpuProfile' || e.name === 'ProfileChunk')) return false
+  if (
+    !rawProfile.find(
+      e => e.name === 'CpuProfile' || e.name === 'Profile' || e.name === 'ProfileChunk',
+    )
+  )
+    return false
   return true
 }
 
