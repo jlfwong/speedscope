802fc2d358240cea6a3147b362b403edb69326be
Jamie Wong <jlfwong@figma.com>
2018-09-09 18:03:49 -0700

    Add test for Chrome 69 import

--- src/import/chrome.test.ts
+++ src/import/chrome.test.ts
@@ -7,3 +7,7 @@ test('importFromChromeCPUProfile', async () => {
 test('importFromChromeTimeline', async () => {
   await checkProfileSnapshot('./sample/profiles/Chrome/65/simple-timeline.json')
 })
+
+test('importFromChromeTimeline Chrome 69', async () => {
+  await checkProfileSnapshot('./sample/profiles/Chrome/69/simple.json')
+})
