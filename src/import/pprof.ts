c43033116c83efe8652cc7cd3a05aaa72c9e639d
Jamie Wong <jlfwong@figma.com>
2018-09-20 23:00:28 -0700

    WIP interpreting the decoded profile.proto data

--- src/import/pprof.ts
+++ src/import/pprof.ts
@@ -4,6 +4,25 @@ export function importAsPprofProfile(rawProfile: ArrayBuffer): void {
   try {
     const protoProfile = perftools.profiles.Profile.decode(new Uint8Array(rawProfile))
     console.log(protoProfile)
+
+    const {sample, sampleType} = protoProfile
+
+    function atIndex<V>(map: {[key: number]: V}, k: any): V {
+      return map[k]
+    }
+
+    function stringVal(key: any) {
+      return atIndex(protoProfile.stringTable, key)
+    }
+
+    const sampleTypes = sampleType.map(type => [stringVal(type.type), stringVal(type.unit)])
+    const preferredType = sampleTypes.find(t => t[1] === 'cpu' || t[1] === 'wall') || sampleTypes[0]
+    const preferredTypeIndex = sampleTypes.indexOf(preferredType)
+
+    for (let s of sample) {
+      const stack = s.locationId ? s.locationId.map(l => atIndex(protoProfile.location, l)) : []
+      console.log(stack, s.value![preferredTypeIndex])
+    }
   } catch (e) {
     // TODO(jlfwong): Remove this console.error
     console.error(e)
