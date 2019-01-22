20cf4edc350adf928602dc5fd576960b4cc1ca6a
Jamie Wong <jlfwong@figma.com>
2019-01-21 18:32:47 -0800

    Update tests to improve branch coverage

--- src/import/trace-event.test.ts
+++ src/import/trace-event.test.ts
@@ -4,6 +4,10 @@ test('importTraceEvents simple', async () => {
   await checkProfileSnapshot('./sample/profiles/trace-event/simple.json')
 })
 
+test('importTraceEvents simple object', async () => {
+  await checkProfileSnapshot('./sample/profiles/trace-event/simple-object.json')
+})
+
 test('importTraceEvents multiprocess', async () => {
   await checkProfileSnapshot('./sample/profiles/trace-event/multiprocess.json')
 })
