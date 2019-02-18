a2022a07a24c6c87d7cb42917976e7c4fff74230
Jamie Wong <jamie.lf.wong@gmail.com>
2019-02-17 18:30:02 -0800

    Fix crash when importing from stackprof without raw_timestamp_deltas (#207)
    Fixes #200

--- src/import/index.test.ts
+++ src/import/index.test.ts
@@ -5,4 +5,8 @@ test('importProfileGroup', async () => {
   expect(await importProfileGroupFromText('unknown', '')).toBe(null)
   expect(await importProfileGroupFromText('unknown', 'Hello world')).toBe(null)
   expect(await importProfileGroupFromText('unknown', 'Hello\n\nWorld')).toBe(null)
+
+  // Importing from a version of stackprof which was missing raw_timestamp_deltas should return null
+  const oldStackprof = `{"version":1.2,"mode":"wall","interval":1000,"samples":0,"gc_samples":0,"missed_samples":0,"frames":{}}`
+  expect(await importProfileGroupFromText('unknown', oldStackprof)).toBe(null)
 })
