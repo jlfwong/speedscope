42e1f14694be029c52852602f5813ddade5a8172
Jamie Wong <jamie.lf.wong@gmail.com>
2020-05-17 00:56:12 -0700

    Clean up ApplicationContainer code a bit

--- src/views/application-container.tsx
+++ src/views/application-container.tsx
@@ -3,7 +3,6 @@ import {Application, ActiveProfileState} from './application'
 import {ApplicationState} from '../store'
 import {getProfileToView, getCanvasContext} from '../store/getters'
 import {actions} from '../store/actions'
-import {Graphics} from '../gl/graphics'
 import {useSelector, useActionCreator} from '../lib/preact-redux'
 import {memo} from 'preact/compat'
 
@@ -13,59 +12,40 @@ export function useAppSelector<T>(selector: (t: ApplicationState) => T, selector
 
 export const ApplicationContainer = memo(() => {
   const appState = useAppSelector(state => state)
-  const flattenRecursion = useAppSelector(state => state.flattenRecursion)
-  const glCanvas = useAppSelector(state => state.glCanvas)
-
-  const activeProfileState: ActiveProfileState | null = useAppSelector(
-    state => {
-      const {profileGroup} = state
-      if (!profileGroup) return null
-      if (profileGroup.indexToView >= profileGroup.profiles.length) return null
-
-      const index = profileGroup.indexToView
-      const profileState = profileGroup.profiles[index]
-      return {
-        ...profileGroup.profiles[profileGroup.indexToView],
-        profile: getProfileToView({profile: profileState.profile, flattenRecursion}),
-        index: profileGroup.indexToView,
-      }
-    },
-    [flattenRecursion],
+  const canvasContext = useAppSelector(state =>
+    state.glCanvas ? getCanvasContext(state.glCanvas) : null,
   )
 
-  // TODO(jlfwong): Cache this and resizeCanvas below to prevent re-renders
-  // due to changing props.
-  const setters = {
-    setGLCanvas: useActionCreator(actions.setGLCanvas),
-    setLoading: useActionCreator(actions.setLoading),
-    setError: useActionCreator(actions.setError),
-    setProfileGroup: useActionCreator(actions.setProfileGroup),
-    setDragActive: useActionCreator(actions.setDragActive),
-    setViewMode: useActionCreator(actions.setViewMode),
-    setFlattenRecursion: useActionCreator(actions.setFlattenRecursion),
-    setProfileIndexToView: useActionCreator(actions.setProfileIndexToView),
-  }
+  const activeProfileState: ActiveProfileState | null = useAppSelector(state => {
+    const {profileGroup} = state
+    if (!profileGroup) return null
+    if (profileGroup.indexToView >= profileGroup.profiles.length) return null
+
+    const index = profileGroup.indexToView
+    const profileState = profileGroup.profiles[index]
+    return {
+      ...profileGroup.profiles[profileGroup.indexToView],
+      profile: getProfileToView({
+        profile: profileState.profile,
+        flattenRecursion: state.flattenRecursion,
+      }),
+      index: profileGroup.indexToView,
+    }
+  })
 
   return (
     <Application
-      {...{
-        ...appState,
-        activeProfileState,
-        canvasContext: glCanvas ? getCanvasContext(glCanvas) : null,
-        resizeCanvas: (
-          widthInPixels: number,
-          heightInPixels: number,
-          widthInAppUnits: number,
-          heightInAppUnits: number,
-        ) => {
-          if (glCanvas) {
-            const gl = getCanvasContext(glCanvas).gl
-            gl.resize(widthInPixels, heightInPixels, widthInAppUnits, heightInAppUnits)
-            gl.clear(new Graphics.Color(1, 1, 1, 1))
-          }
-        },
-        ...setters,
-      }}
+      activeProfileState={activeProfileState}
+      canvasContext={canvasContext}
+      setGLCanvas={useActionCreator(actions.setGLCanvas)}
+      setLoading={useActionCreator(actions.setLoading)}
+      setError={useActionCreator(actions.setError)}
+      setProfileGroup={useActionCreator(actions.setProfileGroup)}
+      setDragActive={useActionCreator(actions.setDragActive)}
+      setViewMode={useActionCreator(actions.setViewMode)}
+      setFlattenRecursion={useActionCreator(actions.setFlattenRecursion)}
+      setProfileIndexToView={useActionCreator(actions.setProfileIndexToView)}
+      {...appState}
     />
   )
 })
