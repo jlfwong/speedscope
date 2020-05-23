d6500587a8c93c0c81d3728c9aff3a71d6bc40b4
Jamie Wong <jamie.lf.wong@gmail.com>
2020-05-23 16:00:56 -0700

    Drop dependency on quicktype, replace with typescript-json-schema

--- scripts/generate-file-format-schema-json.js
+++ scripts/generate-file-format-schema-json.js
@@ -3,7 +3,7 @@ const child_process = require('child_process')
 
 // Convert the file-format-spec.ts file into a json schema file
 let jsonSchema = child_process.execSync(
-  'node_modules/.bin/quicktype --lang schema ./src/lib/file-format-spec.ts',
+  'node_modules/.bin/typescript-json-schema ./src/lib/file-format-spec.ts --titles --required --topRef "*"',
   {
     encoding: 'utf8',
   },
