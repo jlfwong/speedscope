c6f59e6f777cf667aec040efa6d89dc634cfc11d
Jamie Wong <jamie.lf.wong@gmail.com>
2020-07-12 21:12:19 -0700

    Fix styling of search view

--- src/views/search-view.tsx
+++ src/views/search-view.tsx
@@ -2,7 +2,7 @@ import {StyleSheet, css} from 'aphrodite'
 import {h} from 'preact'
 import {useCallback, useRef} from 'preact/hooks'
 import {memo} from 'preact/compat'
-import {Sizes, Colors} from './style'
+import {Sizes, Colors, FontSize} from './style'
 import {useWindowListener} from '../lib/preact-utils'
 
 function stopPropagation(ev: Event) {
@@ -43,11 +43,13 @@ export const SearchView = memo(
     useWindowListener(
       'keydown',
       (ev: KeyboardEvent) => {
+        // Cmd+F or Ctrl+F open the search box
         if (ev.key == 'f' && (ev.metaKey || ev.ctrlKey)) {
           // Prevent the browser's search menu from appearing
           ev.preventDefault()
 
           if (inputRef.current) {
+            // If the search box is already open, then re-select it.
             inputRef.current.select()
           } else {
             setSearchIsActive(true)
@@ -66,10 +68,13 @@ export const SearchView = memo(
       inputRef.current = node
     }, [])
 
+    const close = useCallback(() => setSearchIsActive(false), [setSearchIsActive])
+
     if (!searchIsActive) return null
 
     return (
       <div className={css(style.searchView)}>
+        <span className={css(style.icon)}>🔍</span>
         <input
           className={css(style.input)}
           value={searchQuery}
@@ -79,6 +84,20 @@ export const SearchView = memo(
           onKeyPress={stopPropagation}
           ref={focusInput}
         />
+
+        <svg
+          onClick={close}
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M4.99999 4.16217L11.6427 10.8048M11.6427 4.16217L4.99999 10.8048"
+            stroke="#BDBDBD"
+          />
+        </svg>
       </div>
     )
   },
@@ -94,6 +113,7 @@ const style = StyleSheet.create({
     borderWidth: 2,
     borderColor: Colors.BLACK,
     borderStyle: 'solid',
+    fontSize: FontSize.LABEL,
     boxSizing: 'border-box',
     background: Colors.DARK_GRAY,
     color: Colors.WHITE,
@@ -102,6 +122,8 @@ const style = StyleSheet.create({
   input: {
     border: 'none',
     background: 'none',
+    fontSize: FontSize.LABEL,
+    flex: 1,
     color: Colors.WHITE,
     ':focus': {
       border: 'none',
@@ -111,5 +133,11 @@ const style = StyleSheet.create({
       color: Colors.WHITE,
       background: Colors.DARK_BLUE,
     },
+  },
+  icon: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    paddingTop: '0px',
+    margin: '0 2px 0 4px',
   },
 })
