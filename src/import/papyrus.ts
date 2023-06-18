091e5a86bd834a05015cad1b5634bdc5be97324f
xieve <xieve@mailbox.org>
2023-06-19 00:12:33 +0200

    Parse QUEUE_PUSH event
    Refactored variable naming to be more consistent

--- src/import/papyrus.ts
+++ src/import/papyrus.ts
@@ -1,3 +1,16 @@
+// Papyrus works with a Queue system, because it is multithreaded, but only one thread can exist per script.
+// (And we can only profile one script at a time.) We parse most "QUEUE_PUSH" events, specifically those that come
+// directly before their corresponding "PUSH" event. Other "QUEUE_PUSH" (and all "QUEUE_POP") events come, as far as I
+// can tell, from Events and are thus asynchronous. They are ignored.
+
+// Stack profiling also puts a START operation at the top and a STOP operation at the bottom of the file. These are
+// completely useless to us.
+
+// Stack profiling also logs the Form a method is run on. For those that are not familiar with
+// Papyrus terminology, a "Form" is an instance of a type defined by a script. E.g. a quest is a form that extends
+// the "Quest" script, and thus it has certain methods, like "CompleteQuest()". This information would be useful
+// for Debugging, but for profiling, it would hinder meaningful output in left heavy mode.
+
 import {CallTreeProfileBuilder, Frame, Profile} from '../lib/profile'
 import {KeyedSet} from '../lib/utils'
 import assert from 'assert'
@@ -12,34 +25,52 @@ function lastElement<T>(arr: T[]): T {
 }
 
 export function importFromPapyrus(papyrusProfile: TextFileContent): Profile {
+  const profile = new CallTreeProfileBuilder()
+  profile.setValueFormatter(new TimeFormatter('milliseconds'))
+
   const papyrusProfileLines = papyrusProfile
     .splitLines()
     .filter(line => line.match(/^$|^Log closed$|log opened/) === null)
   const startValue = Number(papyrusProfileLines[0].split(':')[0])
   const endValue = Number(lastElement(papyrusProfileLines).split(':')[0]) - startValue
-  // Profile starts at zero even though I set totalWeight
-  const profile = new CallTreeProfileBuilder()
-  profile.setValueFormatter(new TimeFormatter('milliseconds'))
 
-  // These two are kept in the profile, why are they protected?
   const nameSet: KeyedSet<Frame> = new KeyedSet<Frame>()
   const frameStack: string[] = []
+  let lastEventAt = 0
 
   const leaveASAPStack: string[] = []
+  let lastQueueFrameName: string
+  let lastQueueFrameAt: number = -1
 
-  function enterFrame(at: number, frame: string) {
-    frameStack.push(frame)
-    profile.enterFrame(Frame.getOrInsert(nameSet, {name: frame, key: frame}), at)
+  function enterFrame(at: number, frameName: string) {
+    // Check if the last event was "QUEUE_PUSH"
+    if (lastQueueFrameAt > -1) {
+      // If the queue from last event matches our current frame,
+      if (lastQueueFrameName === frameName && lastQueueFrameAt >= lastEventAt) {
+        // first enter the queue frame at its earlier time
+        enterFrame(lastQueueFrameAt, `QUEUE ${frameName}`)
+      }
+      lastQueueFrameAt = -1
+    }
+    frameStack.push(frameName)
+    profile.enterFrame(Frame.getOrInsert(nameSet, {name: frameName, key: frameName}), at)
+    lastEventAt = at
   }
 
   function leaveFrame(at: number) {
     const frame = frameStack.pop()
     assert(frame !== undefined)
     profile.leaveFrame(Frame.getOrInsert(nameSet, {name: frame, key: frame}), at)
+    // Technically, the frame is popped from queue once it is pushed onto the stack (once we have "entered the frame")
+    // but since we want to visualize meaningfully, we count from QUEUE_PUSH to POP and prefix with "QUEUE "
+    if (frameStack.length > 0 && lastElement(frameStack).startsWith('QUEUE ')) {
+      leaveFrame(at)
+    }
+    lastEventAt = at
   }
 
-  function tryToLeaveFrame(at: number, frame: string): Boolean {
-    if (lastElement(frameStack) === frame) {
+  function tryToLeaveFrame(at: number, frameName: string): Boolean {
+    if (lastElement(frameStack) === frameName) {
       leaveFrame(at)
       // Every time we successfully leave a frame, try to leave from the last frame we put on the leaveASAPStack.
       // If we don't succeed, the frame is pushed back on the leaveASAPStack.
@@ -47,16 +78,16 @@ export function importFromPapyrus(papyrusProfile: TextFileContent): Profile {
       if (leaveASAPFrame !== undefined) tryToLeaveFrame(at, leaveASAPFrame)
       return true
     } else {
-      if (frameStack.includes(frame)) {
-        leaveASAPStack.push(frame)
+      if (frameStack.includes(frameName)) {
+        leaveASAPStack.push(frameName)
 
         console.log(
-          `Tried to leave frame "${frame}" while "${lastElement(
+          `Tried to leave frame "${frameName}" while "${lastElement(
             frameStack,
           )}" was at top. Will continue to try leaving in next iteration.`,
         )
       } else {
-        console.log(`Tried to leave frame "${frame}" which was never entered. Ignoring line.`)
+        console.log(`Tried to leave frame "${frameName}" which was never entered. Ignoring line.`)
       }
       return false
     }
@@ -66,39 +97,30 @@ export function importFromPapyrus(papyrusProfile: TextFileContent): Profile {
     const line_arr = line_str.split(':')
     if (line_arr.length < 6) return // continue
     const at = Number(line_arr[0]) - startValue
-    const operation = line_arr[1]
-    const stack_str = `Stack ${line_arr[2]}`
+    const event = line_arr[1]
+    const stackStr = `STACK ${line_arr[2]}`
     const name = line_arr[5]
-    if (operation === 'PUSH') {
+    if (event === 'PUSH') {
       enterFrame(at, name)
-    } else if (operation === 'POP') {
+    } else if (event === 'POP') {
       // If we can't leave the frame, continue
       if (!tryToLeaveFrame(at, name)) return
+    } else if (event === 'QUEUE_PUSH') {
+      lastQueueFrameName = name.replace(/\?/g, '')
+      lastQueueFrameAt = at
     }
     if (frameStack.length === 0) {
-      enterFrame(at, stack_str)
+      enterFrame(at, stackStr)
     } else if (
       lastElement(frameStack).startsWith('Stack ') &&
-      lastElement(frameStack) !== stack_str
+      lastElement(frameStack) !== stackStr
     ) {
       tryToLeaveFrame(at, lastElement(frameStack))
-      enterFrame(at, stack_str)
+      enterFrame(at, stackStr)
     }
-    // There are other types of operations, namely QUEUE_PUSH and QUEUE_POP. Papyrus works with a Queue system, because
-    // it is multithreaded, but only one thread can exist per script. (And we can only profile one script at a time.)
-    // These events are not very interesting to us, as they are 1. asynchronous and 2. not relevant to measuring the
-    // performance of a specific function.
-
-    // Stack profiling also puts a START operation at the top and a STOP operation at the bottom of the file. These are
-    // completely useless to us.
-
-    // Stack profiling also logs the Form a method is run on. For those that are not familiar with
-    // Papyrus terminology, a "Form" is an instance of a type defined by a script. E.g. a quest is a form that extends
-    // the "Quest" script, and thus it has certain methods, like "CompleteQuest()". This information would be useful
-    // for Debugging, but for profiling, it would hinder meaningful output in left heavy mode.
   })
 
-  // close frames that are still open
+  // Close frames that are still open
   while (frameStack.length > 0) {
     leaveFrame(endValue)
   }
