8e0fa58d65ca0c77a860ac73d746464285246513
Jamie Wong <jamie.lf.wong@gmail.com>
2023-12-25 21:22:56 -0500

    Re-enable eslint prettier rule after being accidentally disabled for 3 years (#454)
    It looks like in #267 (which was 3 years ago!), I accidentally disabled prettier linting altogether 😱
    
    https://github.com/jlfwong/speedscope/pull/267/files#diff-e2954b558f2aa82baff0e30964490d12942e0e251c1aa56c3294de6ec67b7cf5
    
    There's no comment in that PR about this being an intentional thing, so I have to assume this was a dumb mistake.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: ['prettier', '@typescript-eslint', 'react-hooks'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'error',
